1b489371f8bb92c4d4233427e6ffbb3e3df93cd3
Hayden Kong <115069692+haydenkong@users.noreply.github.com>
2025-06-07 21:37:30 +0930

    Add LaTeX Math Support

--- components/messages/message-markdown.tsx
+++ components/messages/message-markdown.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useEffect, useState } from "react"
+import React, { FC } from "react"
 import remarkGfm from "remark-gfm"
 import remarkMath from "remark-math"
+import rehypeKatex from "rehype-katex"
 import { MessageCodeBlock } from "./message-codeblock"
 import { MessageMarkdownMemoized } from "./message-markdown-memoized"
 import { cn } from "@/lib/utils"
+import "katex/dist/katex.min.css"
 
 interface MessageMarkdownProps {
   content: string;
@@ -34,6 +36,7 @@ export const MessageMarkdown: FC<MessageMarkdownProps> = ({
       <MessageMarkdownMemoized
         className="prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 min-w-full space-y-6 break-words"
         remarkPlugins={[remarkGfm, remarkMath]}
+        rehypePlugins={[rehypeKatex]}
         components={{
           p({ children }) {
             return <p className="mb-2 last:mb-0">{children}</p>
